79beea21fa1b49fb46a1a29ff380c73c75f9f352
Ughur <hello@hackermail.com>
2025-07-04 00:14:12 -0700

    Update Markdown component styles for headings to improve visual hierarchy and responsiveness

--- app/blog/Markdown.tsx
+++ app/blog/Markdown.tsx
@@ -12,13 +12,13 @@ const Markdown = ({ content }: { content: string }) => {
       rehypePlugins={[rehypeRaw]}
       components={{
         h1: ({ ...props }) => (
-          <h1 className='text-3xl md:text-4xl font-bold my-4' {...props} />
+          <h1 className='text-xl md:text-3xl font-bold my-4' {...props} />
         ),
         h2: ({ ...props }) => (
-          <h2 className='text-2xl md:text-3xl font-bold my-4' {...props} />
+          <h2 className='text-lg md:text-2xl font-bold my-4' {...props} />
         ),
         h3: ({ ...props }) => (
-          <h3 className='text-xl md:text-2xl font-bold my-4' {...props} />
+          <h3 className='text-sm md:text-xl font-bold my-4' {...props} />
         ),
         p: ({ ...props }) => (
           <p className='text-base md:text-lg my-4' {...props} />
